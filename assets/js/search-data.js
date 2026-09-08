c4d05fd05eb299403083b2b641e14b5c2eda70fe
psychwangzihao <1022182060@qq.com>
2026-09-08 03:06:35 +0000

    Deploying to gh-pages from @ psychwangzihao/psychwangzihao.github.io@c59a91eef9d9ab356a964cff1f9f65bf5aca0c0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,6 +52,9 @@ ninja.data = [{
           section: "News",},{id: "news-invited-by-westlake-university-and-the-joint-academy-on-future-humanity-to-participate-in-the-future-civilization-sandbox-as-a-theme-advocate-leading-the-discussion-topic-decoding-social-interaction-what-ai-can-and-cannot-replace",
           title: 'Invited by Westlake University and the Joint Academy on Future Humanity to participate...',
           description: "",
+          section: "News",},{id: "news-preparing-an-interactive-talk-connecting-the-dots-for-the-incoming-cohort-of-the-department-of-psychology-zhejiang-university-an-interactive-visual-journey-across-interdisciplinary-science-the-2050-gathering-studying-abroad-and-consciousness-research",
+          title: 'Preparing an interactive talk, Connecting the Dots, for the incoming cohort of the...',
+          description: "",
           section: "News",},{
         id: 'social-cv',
         title: 'CV',
